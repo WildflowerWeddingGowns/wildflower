f54de7ed08467999138b6d018cdd11a61aae1403
Corey McGarr <coreymcgarr4@gmail.com>
2017-05-05 15:22:58 -0600

    added parameter requesting 200 responses to API call

--- server/controller/googleAPI.js
+++ server/controller/googleAPI.js
@@ -4,7 +4,7 @@ const config = require('../.config.js'),
 module.exports = {
 
   googleAPICall: function(req, res){
-    axios.get('https://www.googleapis.com/customsearch/v1?q=' + req.params.dressType + '&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006157088675124412018%3Alrfiqnpf6xw&cx=006157088675124412018%3Alrfiqnpf6xw&imgSize=medium&searchType=image&key=' + config.GOOGLEKEY)
+    axios.get('https://www.googleapis.com/customsearch/v1?q=' + req.params.dressType + '&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006157088675124412018%3Alrfiqnpf6xw&cx=006157088675124412018%3Alrfiqnpf6xw&num=200&imgSize=medium&searchType=image&key=' + config.GOOGLEKEY)
     .then(function(response){
       res.send(response.data.items)
     })
