9dac03ee3ee7ea5a27784c87f375959df446cd6c
Corey McGarr <coreymcgarr4@gmail.com>
2017-05-15 10:22:55 -0600

    changed size of image when clicked, added api key

--- server/controller/googleAPI.js
+++ server/controller/googleAPI.js
@@ -12,7 +12,7 @@ module.exports = {
       start = 1
     }
     dressType=req.params.dressType;
-    axios.get('https://www.googleapis.com/customsearch/v1?q=' + req.params.dressType + '&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006157088675124412018%3Alrfiqnpf6xw&cx=006157088675124412018%3Alrfiqnpf6xw&start=' + start + '&imgSize=large&searchType=image&key=' + GOOGLEKEY)
+    axios.get('https://www.googleapis.com/customsearch/v1?q=' + req.params.dressType + '&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006157088675124412018%3Alrfiqnpf6xw&cx=006157088675124412018%3Alrfiqnpf6xw&start=' + start + '&imgSize=xxlarge&searchType=image&key=' + config.GOOGLEKEY)
     .then(function(response){
       res.send(response.data.items)
       start = start + 10
